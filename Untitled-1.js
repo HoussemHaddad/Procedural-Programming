d19271029b59b287763718fd4565cd5a434363ab
HoussemHaddad <haddadhouss2019@gmail.com>
2023-01-29 14:49:36 +0100

    ff

---
+++ Untitled-1.js
@@ -0,0 +1,27 @@
+<script>
+
+
+let n = 3;
+	function dotProduct(vect_A, vect_B){
+    let result = 0;
+		for (let i = 0; i < n; i++)
+			product = product + vect_A[i] * vect_B[i]
+                if(result==0){
+                    return result===orthogonal}
+                else{
+                return result!=orthogonal
+                }
+		return product;
+	}
+	function crossProduct(vect_A, vect_B,cross_P){
+        let ps =0;
+        for (let i = 0; i < n; i++)
+        ps =(v1[i] * v2[i])
+        return ps
+    }
+
+
+			
+			
+			
+</script>
